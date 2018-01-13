a8885ab4cf736d09c3d194c58dfe2c4cab728f82
David Knott <david1k1nott@gmail.com>
2018-01-13 12:32:32 -0700

    Create root chain deployer

---
+++ plasma/root_chain/deployer.py
@@ -0,0 +1,54 @@
+import json
+import web3
+import os
+from ethereum.tools import tester as t
+from ethereum.tools import _solidity
+from solc import compile_standard
+from web3.contract import ConciseContract
+from web3 import Web3, HTTPProvider
+
+OWN_DIR = os.path.dirname(os.path.realpath(__file__))
+
+class Deployer(object):
+
+    def __init__(self, provider=HTTPProvider('http://localhost:8545')):
+        self.w3 = Web3(provider)
+
+    def get_dirs(self,path):
+        abs_contract_path = os.path.realpath(os.path.join(OWN_DIR, '..', 'root_chain', 'contracts'))
+        extra_args = [[file, [os.path.realpath(os.path.join(r,file))]] for r,d,f in os.walk(abs_contract_path) for file in f]
+        contracts = {}
+        for contract in extra_args:
+            contracts[contract[0]] = {'urls': contract[1]}
+        path = '{}/{}'.format(abs_contract_path, path)
+        return path, contracts
+
+    def compile_contract(self, path, args=()):
+        contract_name = path.split('/')[1]
+        contract_name += ':' + contract_name.split('.')[0]
+        path, contracts = self.get_dirs(path)
+        # if args:
+        #     args = [x.address if isinstance(x, t.ABIContract) else x for x in args]
+
+        compiled_sol = compile_standard(
+                        {'language': 'Solidity',
+                            'sources': {**{path.split('/')[-1]: {'urls': [path]}}, **contracts},
+                            },
+                        allow_paths="/Users/Knott/coding_jobs/omisego/plasma_mvp/simple_plasma/plasma/root_chain/contracts")
+        abi = compiled_sol['contracts']['RootChain.sol']['RootChain']['abi']
+        bytecode = compiled_sol['contracts']['RootChain.sol']['RootChain']['evm']['bytecode']['object']
+        return abi, bytecode
+
+    def create_contract(self, path, args=(), sender=t.k0):
+        abi, bytecode = self.compile_contract(path, args)
+        contract= self.w3.eth.contract(abi=abi, bytecode=bytecode)
+        # Get transaction hash from deployed contract
+        tx_hash = contract.deploy(transaction={'from': self.w3.eth.accounts[0], 'gas': 4410000}, args=args)
+
+        # Get tx receipt to get contract address
+        tx_receipt = self.w3.eth.getTransactionReceipt(tx_hash)
+        contract_address = tx_receipt['contractAddress']
+
+        # Contract instance in concise mode
+        contract_instance = self.w3.eth.contract(abi, contract_address, ContractFactoryClass=ConciseContract)
+        return contract_instance
