0ec509122c5011ab5cef86b8819de7006f14d994
David Knott <david1k1nott@gmail.com>
2018-01-13 12:33:12 -0700

    Improve conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,50 +1,41 @@
 import pytest
 import rlp
-from ethereum.tools import tester
+from ethereum.tools import tester, _solidity
+from ethereum.abi import ContractTranslator
 from ethereum import transactions, messages, utils
 from plasma.utils import utils as plasma_utils
+from plasma.root_chain.deployer import Deployer
 from viper import utils as viper_utils
+import os
 
+
+OWN_DIR = os.path.dirname(os.path.realpath(__file__))
 
 @pytest.fixture
 def t():
     tester.chain = tester.Chain()
     return tester
 
-@pytest.fixture
-def inject_tx(u, t):
-    def inject_tx(txhex):
-        tx = rlp.decode(utils.decode_hex(txhex[2:]), transactions.Transaction)
-        t.chain.head_state.set_balance(tx.sender, tx.startgas * tx.gasprice)
-        t.chain.chain.state.set_balance(tx.sender, tx.startgas * tx.gasprice)
-        messages.apply_transaction(t.chain.head_state, tx)
-        t.chain.block.transactions.append(tx)
-        contract_address = utils.sha3(rlp.encode([tx.sender, 0]))[12:]
-        assert t.chain.head_state.get_code(contract_address)
-        t.chain.mine(1)
-        t.chain.head_state.gas_limit = 10**9
-        return contract_address
-    return inject_tx
+def get_dirs(path):
+    abs_contract_path = os.path.realpath(os.path.join(OWN_DIR, '..', 'plasma', 'root_chain', 'contracts'))
+    sub_dirs = [x[0] for x in os.walk(abs_contract_path)]
+    extra_args = ' '.join(['{}={}'.format(d.split('/')[-1], d) for d in sub_dirs])
+    path = '{}/{}'.format(abs_contract_path, path)
+    return path, extra_args
 
-@pytest.fixture
-def rlp_decoder_tx(inject_tx):
-    def rlp_decoder_tx():
-        address = inject_tx("0xf9035b808506fc23ac0083045f788080b903486103305660006109ac5260006109cc527f0100000000000000000000000000000000000000000000000000000000000000600035046109ec526000610a0c5260006109005260c06109ec51101515585760f86109ec51101561006e5760bf6109ec510336141558576001610a0c52610098565b60013560f76109ec51036020035260005160f66109ec510301361415585760f66109ec5103610a0c525b61022060016064818352015b36610a0c511015156100b557610291565b7f0100000000000000000000000000000000000000000000000000000000000000610a0c5135046109ec526109cc5160206109ac51026040015260016109ac51016109ac5260806109ec51101561013b5760016109cc5161044001526001610a0c516109cc5161046001376001610a0c5101610a0c5260216109cc51016109cc52610281565b60b86109ec5110156101d15760806109ec51036109cc51610440015260806109ec51036001610a0c51016109cc51610460013760816109ec5114156101ac5760807f01000000000000000000000000000000000000000000000000000000000000006001610a0c5101350410151558575b607f6109ec5103610a0c5101610a0c5260606109ec51036109cc51016109cc52610280565b60c06109ec51101561027d576001610a0c51013560b76109ec510360200352600051610a2c526038610a2c5110157f01000000000000000000000000000000000000000000000000000000000000006001610a0c5101350402155857610a2c516109cc516104400152610a2c5160b66109ec5103610a0c51016109cc516104600137610a2c5160b66109ec5103610a0c510101610a0c526020610a2c51016109cc51016109cc5261027f565bfe5b5b5b81516001018083528114156100a4575b5050601f6109ac511115155857602060206109ac5102016109005260206109005103610a0c5261022060016064818352015b6000610a0c5112156102d45761030a565b61090051610a0c516040015101610a0c51610900516104400301526020610a0c5103610a0c5281516001018083528114156102c3575b50506109cc516109005101610420526109cc5161090051016109005161044003f35b61000461033003610004600039610004610330036000f31b2d4f")
-        assert viper_utils.bytes_to_int(address) == viper_utils.RLP_DECODER_ADDRESS
-        return address
-    return rlp_decoder_tx
-
-@pytest.fixture
-def u():
-    utils.plasma = plasma_utils
-    return utils
+def create_abi(path):
+    path, extra_args = get_dirs(path)
+    abi = _solidity.compile_last_contract(path, combined='abi', extra_args=extra_args)['abi']
+    return ContractTranslator(abi)
 
 
 @pytest.fixture
-def bytes_helper():
-    def bytes_helper(inp, length):
-        return  bytes(length-len(inp)) + inp
-    return bytes_helper
+def assert_failed(t):
+    def assert_failed(function_to_test, exception):
+        with pytest.raises(exception):
+            function_to_test()
+    return assert_failed
+
 
 @pytest.fixture
 def assert_tx_failed(t):
@@ -55,8 +46,26 @@ def assert_tx_failed(t):
         t.chain.revert(initial_state)
     return assert_tx_failed
 
+
 @pytest.fixture
-def get_contract(t):
-    def get_contract(source_code, *args, **kwargs):
-        return t.chain.contract(source_code, language="viper", *args, **kwargs)
-    return get_contract
+def u():
+    utils.plasma = plasma_utils
+    return utils
+
+@pytest.fixture
+def get_contract(t, u):
+    def create_contract(path, args=(), sender=t.k0):
+        abi, hexcode = Deployer().compile_contract(path, args)
+        bytecode = u.decode_hex(hexcode)
+        ct = ContractTranslator(abi)
+        code = bytecode + (ct.encode_constructor_arguments(args) if args else b'')
+        address = t.chain.tx(sender=sender, to=b'', startgas=4*10**6, value=0, data=code)
+        return t.ABIContract(t.chain, abi, address)
+    return create_contract
+
+
+@pytest.fixture
+def bytes_helper():
+    def bytes_helper(inp, length):
+        return  bytes(length-len(inp)) + inp
+    return bytes_helper
