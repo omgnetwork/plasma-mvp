212626cf3a65b36bc8bbd273b8d89f4ee5bf4be7
David Knott <david1k1nott@gmail.com>
2018-01-13 12:31:06 -0700

    Add merkle checker library

---
+++ plasma/root_chain/contracts/Libraries/Merkle.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.4.18;
+
+
+library Merkle {
+    function checkMembership(bytes32 leaf, uint256 index, bytes32 rootHash, bytes proof)
+        internal
+        pure
+        returns (bool)
+    {
+        require(proof.length == 512);
+        bytes32 proofElement;
+        bytes32 computedHash = leaf;
+
+        for (uint256 i = 32; i <= 512; i += 32) {
+            assembly {
+                proofElement := mload(add(proof, i))
+            }
+            if (index % 2 == 0) {
+                computedHash = keccak256(computedHash, proofElement);
+            } else {
+                computedHash = keccak256(proofElement, computedHash);
+            }
+            index = index / 2;
+        }
+        return computedHash == rootHash;
+    }
+}
