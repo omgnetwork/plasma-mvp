279b974ae1fb2c78c48f9c59e05c2f1a21aed07b
David Knott <david1k1nott@gmail.com>
2018-01-17 10:48:22 -0700

    Add root chain tests for finalize exits

--- tests/root_chain/contracts/root_chain/test_root_chain.py
+++ tests/root_chain/contracts/root_chain/test_root_chain.py
@@ -24,13 +24,14 @@ def test_deposit(t, root_chain):
     assert root_chain.currentChildBlock() == 2
 
 
-def test_start_exit(t, root_chain):
+def test_start_exit(t, root_chain, assert_tx_failed):
+    week_and_a_half = 60 * 60 * 24 * 13
     owner, value_1, key = t.a1, 100, t.k1
     null_address = b'\x00' * 20
     tx1 = Transaction(0, 0, 0, 0, 0, 0,
                 owner, value_1, null_address, 0, 0)
     tx_bytes1 = rlp.encode(tx1, UnsignedTransaction)
-    yo = root_chain.deposit(tx_bytes1, value=value_1)
+    root_chain.deposit(tx_bytes1, value=value_1)
     merkle = FixedMerkle(16, [tx1.merkle_hash], True)
     proof = merkle.create_membership_proof(tx1.merkle_hash)
     confirmSig1 = confirm_tx(tx1, root_chain.getChildChain(1)[0], key)
@@ -39,6 +40,9 @@ def test_start_exit(t, root_chain):
     sigs = tx1.sig1 + tx1.sig2 + confirmSig1
     # Deposit exit
     root_chain.startExit([1, 0, 0], tx_bytes1, proof, sigs, sender=key)
+    t.chain.head_state.timestamp += week_and_a_half
+    # Cannot exit twice off of the same utxo
+    assert_tx_failed(lambda:root_chain.startExit([1, 0, 0], tx_bytes1, proof, sigs, sender=key));
     assert root_chain.getExit(priority1) == ['0x' + owner.hex(), 100, [1, 0, 0]]
     t.chain.revert(snapshot)
 
@@ -88,7 +92,6 @@ def test_challenge_exit(t, u, root_chain):
     merkle = FixedMerkle(16, [tx1.merkle_hash], True)
     proof = merkle.create_membership_proof(tx1.merkle_hash)
     confirmSig1 = confirm_tx(tx1, root_chain.getChildChain(1)[0], key)
-    priority = 1 * 1000000000 + 10000 * 0 + 0
     sigs = tx1.sig1 + tx1.sig2 + confirmSig1
     root_chain.startExit([1, 0, 0], tx_bytes1, proof, sigs, sender=key)
     tx2 = Transaction(1, 0, 0, 0, 0, 0,
@@ -101,6 +104,34 @@ def test_challenge_exit(t, u, root_chain):
     root_chain.submitBlock(merkle.root)
     confirmSig = confirm_tx(tx2, root_chain.getChildChain(2)[0], key)
     sigs = tx2.sig1 + tx2.sig2
-    assert root_chain.exits(priority) == ['0x' + owner.hex(), 100]
-    root_chain.challengeExit(priority, [2, 0, 0], tx_bytes2, proof, sigs, confirmSig)
-    assert root_chain.exits(priority) == ['0x0000000000000000000000000000000000000000', 0]
+    exit_id = 1 * 1000000000 + 10000 * 0 + 0
+    assert root_chain.exits(exit_id) == ['0x' + owner.hex(), 100]
+    assert root_chain.exitIds(exit_id) == exit_id
+    root_chain.challengeExit(exit_id, [2, 0, 0], tx_bytes2, proof, sigs, confirmSig)
+    assert root_chain.exits(exit_id) == ['0x0000000000000000000000000000000000000000', 0]
+    assert root_chain.exitIds(exit_id) == 0
+
+
+def test_finalize_exits(t, u, root_chain):
+    two_weeks = 60 * 60 * 24 * 14
+    owner, value_1, key = t.a1, 100, t.k1
+    null_address = b'\x00' * 20
+    tx1 = Transaction(0, 0, 0, 0, 0, 0,
+                owner, value_1, null_address, 0, 0)
+    tx_bytes1 = rlp.encode(tx1, UnsignedTransaction)
+    root_chain.deposit(tx_bytes1, value=value_1)
+    merkle = FixedMerkle(16, [tx1.merkle_hash], True)
+    proof = merkle.create_membership_proof(tx1.merkle_hash)
+    confirmSig1 = confirm_tx(tx1, root_chain.getChildChain(1)[0], key)
+    sigs = tx1.sig1 + tx1.sig2 + confirmSig1
+    root_chain.startExit([1, 0, 0], tx_bytes1, proof, sigs, sender=key)
+    exit_id = 1 * 1000000000 + 10000 * 0 + 0
+    t.chain.head_state.timestamp += two_weeks * 2
+    assert root_chain.exits(exit_id) == ['0x' + owner.hex(), 100]
+    assert root_chain.exitIds(exit_id) == exit_id
+    pre_balance = t.chain.head_state.get_balance(owner)
+    root_chain.finalizeExits(sender=t.k2)
+    post_balance = t.chain.head_state.get_balance(owner)
+    assert post_balance == pre_balance + value_1
+    assert root_chain.exits(exit_id) == ['0x0000000000000000000000000000000000000000', 0]
+    assert root_chain.exitIds(exit_id) == 0
