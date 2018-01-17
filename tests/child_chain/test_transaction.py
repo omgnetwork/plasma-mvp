17f01e162adb9d4000867a36eaedf087ab1a99b8
David Knott <david1k1nott@gmail.com>
2018-01-17 11:22:48 -0700

    Update transaction tests

--- tests/child_chain/test_transaction.py
+++ tests/child_chain/test_transaction.py
@@ -1,6 +1,5 @@
 import pytest
 import rlp
-from plasma.child_chain.utxo import Utxo
 from plasma.child_chain.transaction import Transaction, UnsignedTransaction
 from plasma.config import plasma_config
 
@@ -24,21 +23,13 @@ def test_transaction(t, u, assert_failed):
     assert tx.blknum2 == blknum2
     assert txindex2 == txindex2
     assert tx.oindex2 == oindex2
-    assert tx.utxos[0].owner == newowner1
-    assert tx.utxos[0].amount == amount1
-    assert tx.utxos[1].owner == newowner2
-    assert tx.utxos[1].amount == amount2
-    assert tx.sig1 == [0, 0, 0]
-    assert tx.sig2 == [0, 0, 0]
-    tx_vars = [blknum1, txindex1, oindex1,
-                blknum2, txindex2, oindex2,
-                newowner1, amount1,
-                newowner2, amount2,
-                fee]
-    assert_failed(lambda: tx.sender1, ValueError)
-    assert_failed(lambda: tx.sender2, ValueError)
+    assert tx.newowner1 == newowner1
+    assert tx.amount1 == amount1
+    assert tx.newowner2 == newowner2
+    assert tx.amount2 == amount2
+    assert tx.sig1 == b'\x00' * 65
+    assert tx.sig2 == b'\x00' * 65
     tx.sign1(key1)
     assert tx.sender1 ==  oldowner1
-    assert_failed(lambda: tx.sender2, ValueError)
     tx.sign2(key2)
     assert tx.sender2 ==  oldowner2
