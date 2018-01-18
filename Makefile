67bdae43c8957f62e7cf599b40d0f67c03e57a9b
David Knott <david1k1nott@gmail.com>
2018-01-17 21:44:34 -0700

    Test block

--- Makefile
+++ Makefile
@@ -24,6 +24,7 @@ clean-pyc:
 	find . -name '*.pyc' -exec rm -f {} +
 	find . -name '*.pyo' -exec rm -f {} +
 	find . -name '*~' -exec rm -f {} +
+	find . -name '*pycache__' -exec rm -rf {} +
 
 .PHONY: lint
 lint:
---
+++ tests/child_chain/test_block.py
@@ -0,0 +1,20 @@
+import pytest
+from plasma.child_chain.block import Block, UnsignedBlock
+from plasma.utils.utils import sign, get_sender
+
+
+@pytest.fixture
+def block():
+    return Block()
+
+
+def test_initial_state(block):
+    block.transaction_set = []
+    block.sig = b'\x00' * 65
+    block.merkle = None
+
+
+def test_signature(t, block):
+    block.sign(t.k0)
+    assert block.sig ==  sign(block.hash, t.k0)
+    assert  block.sender == get_sender(block.hash, sign(block.hash, t.k0))
