283d87e70cac6d17a9c9651174ab021891fd823c
David Knott <david1k1nott@gmail.com>
2018-02-07 16:22:23 -0700

    Update config

--- plasma/config.py
+++ plasma/config.py
@@ -1,8 +1,5 @@
-from ethereum.tools import tester as t
-
-
 plasma_config = dict(
-    root_chain_address=None,
-    AUTHORITY=t.a0,
-    AUTHORITY_KEY=t.k0,
+    ROOT_CHAIN_CONTRACT_ADDRESS="0xa3b2a1804203b75b494028966c0f62e677447a39",
+    AUTHORITY=b'\xfd\x02\xec\xeeby~u\xd8k\xcf\xf1d.\xb0\x84J\xfb(\xc7',
+    AUTHORITY_KEY=b';\xb3i\xfe\xcd\xc1k\x93\xb9\x95\x14\xd8\xed\x9c.\x87\xc5\x82L\xf4\xa6\xa9\x8d.\x8e\x91\xb7\xdd\x0c\x063\x04',
 )
